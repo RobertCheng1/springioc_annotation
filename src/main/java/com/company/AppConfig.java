9169e892b0b3127caefcfbd0b507cdc66bcf3819
RobertCheng <cpxzaitaiyuan@126.com>
2021-04-05 16:33:54 +0800

    Move the aop comments,For details Please refer to springaop project

--- src/main/java/com/company/AppConfig.java
+++ src/main/java/com/company/AppConfig.java
@@ -13,7 +13,6 @@ import java.time.ZoneId;
 @Configuration //表示该类是一个配置类，因为我们创建ApplicationContext时：
 @ComponentScan //告诉容器，自动搜索当前类所在的包以及子包，把所有标注为 @Component 的Bean自动创建出来，并根据 @Autowired 进行装配。
 @PropertySource("app.properties") // 表示读取classpath的app.properties
-@EnableAspectJAutoProxy
 public class AppConfig {
 	// 定制 Bean--创建第三方Bean:
 	// 如果一个Bean不在我们自己的package管理之内，例如ZoneId，如何创建它？
@@ -49,7 +48,6 @@ public class AppConfig {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
 		/**
 		 * 从 Spring开发--IoC容器--使用 Annotation 配置 开始：
 		 * 使用Spring的IoC容器，实际上就是通过类似XML这样的配置文件，把我们自己的Bean的依赖关系描述出来，然后让容器来创建并装配Bean。
@@ -174,74 +172,6 @@ public class AppConfig {
 		// 测试 IoC容器--使用Resource：
 		AppService appService = context.getBean(AppService.class);
 		appService.printLogo();
-
-
-		// 测试 使用AOP
-		/**
-		 * 要理解AOP的概念，我们先用OOP举例，比如一个业务组件BookService，它有几个业务方法：
-		 *     createBook：添加新的Book；
-		 *     updateBook：修改Book；
-		 *     deleteBook：删除Book。
-		 * 对每个业务方法，例如，createBook()，除了业务逻辑，还需要安全检查、日志记录和事务处理.
-		 * 对于安全检查、日志、事务等代码，它们会重复出现在每个业务方法中。使用OOP，我们很难将这些四处分散的代码模块化。
-		 *
-		 * AOP原理:
-		 * 如何把切面织入到核心逻辑中？这正是AOP需要解决的问题。换句话说，如果客户端获得了BookService的引用，
-		 * 当调用bookService.createBook()时，如何对调用方法进行拦截，并在拦截前后进行安全检查、日志、事务等处理，就相当于完成了所有业务功能。
-		 *
-		 * 在Java平台上，对于AOP的织入，有3种方式：
-		 *     编译期：在编译时，由编译器把切面调用编译进字节码，这种方式需要定义新的关键字并扩展编译器，
-		 *     		  AspectJ就扩展了Java编译器，使用关键字aspect来实现织入；
-		 *     类加载器：在目标类被装载到JVM时，通过一个特殊的类加载器，对目标类的字节码重新“增强”；
-		 *     运行期：目标对象和切面都是普通Java类，通过JVM的动态代理功能或者第三方库实现运行期动态织入。
-		 * 最简单的方式是第三种，Spring的AOP实现就是基于JVM的动态代理。
-		 * 由于JVM的动态代理要求必须实现接口，如果一个普通类没有业务接口，就需要通过CGLIB或者Javassist这些第三方库实现。
-		 * AOP技术看上去比较神秘，但实际上，它本质就是一个动态代理，让我们把一些常用功能如权限检查、日志、事务等，从每个业务方法中剥离出来。
-		 *
-		 *
-		 * Spring的IoC容器看到这个（@EnableAspectJAutoProxy）注解，就会自动查找带有 @Aspect 的Bean，
-		 * 然后根据每个方法的@Before、@Around等注解把AOP注入到特定的Bean中。执行代码，我们可以看到以下输出。
-		 *
-		 * 有些童鞋会问，LoggingAspect定义的方法，是如何注入到其他Bean的呢？===这是个好问题===
-		 * 其实AOP的原理非常简单。我们以LoggingAspect.doAccessCheck()为例，要把它注入到UserService的每个public方法中，
-		 * 最简单的方法是编写一个子类，并持有原始实例的引用：
-		 * 		public UserServiceAopProxy extends UserService {
-		 * 		    private UserService target;
-		 * 		    private LoggingAspect aspect;
-		 *
-		 * 		    public UserServiceAopProxy(UserService target, LoggingAspect aspect) {
-		 * 		        this.target = target;
-		 * 		        this.aspect = aspect;
-		 * 		    }
-		 *
-		 * 		    public User login(String email, String password) {
-		 * 		        // 先执行Aspect的代码:
-		 * 		        aspect.doAccessCheck();
-		 * 		        // 再执行UserService的逻辑:
-		 * 		        return target.login(email, password);
-		 * 		    }
-		 *
-		 * 		    public User register(String email, String password, String name) {
-		 * 		        aspect.doAccessCheck();
-		 * 		        return target.register(email, password, name);
-		 * 		    }
-		 * 		    ...
-		 * 		}
-		 * 这些都是Spring容器启动时为我们自动创建的注入了Aspect的子类，它取代了原始的UserService（原始的UserService实例作为
-		 * 内部变量隐藏在UserServiceAopProxy中）。如果我们打印从Spring容器获取的UserService实例类型，它类似
-		 * UserService$$EnhancerBySpringCGLIB$$1f44e01c，实际上是Spring使用CGLIB动态创建的子类，但对于调用方来说，感觉不到任何区别。
-		 * Spring对接口类型使用JDK动态代理，对普通类使用CGLIB创建子类。如果一个Bean的class是final，Spring将无法为其创建子类。
-		 * 虽然Spring容器内部实现AOP的逻辑比较复杂（需要使用 AspectJ 解析注解，并通过CGLIB实现代理类），===说到本质了===
-		 * 但我们使用AOP非常简单，一共需要三步：
-		 *     定义执行方法，并在方法上通过 AspectJ 的注解告诉 Spring 应该在何处调用此方法；
-		 *     标记 @Component 和 @Aspect；
-		 *     在 @Configuration 类上标注 @EnableAspectJAutoProxy。
-		 * 至于AspectJ的注入语法则比较复杂，请参考Spring文档。
-		 * Spring也提供其他方法来装配AOP，但都没有使用AspectJ注解的方式来得简洁明了，所以我们不再作介绍。
-		 *
-		 * 使用AOP--装配 AOP的小结：
-		 * 		在Spring容器中使用AOP非常简单，只需要定义执行方法，并用AspectJ的注解标注应该在何处触发并执行。
-		 * 		Spring通过CGLIB动态创建子类等方式来实现AOP代理模式，大大简化了代码。
-		 */
 	}
 }
+
